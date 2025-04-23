4cb983b33ad2f6046a6090fb9a8220668194189a
striker1101 <goodluckodemilin@gmail.com>
2025-04-23 15:14:21 +0100

    update chat

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -8,7 +8,7 @@ const TawkToChat = () => {
       var s1 = document.createElement("script"),
         s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/67c39d37252eb9190ae0e51a/1ila1s8ur";
+      s1.src = "https://embed.tawk.to/6808f54c753e2219109a9cf3/1iphfsd6f";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
